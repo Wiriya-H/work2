f7d9092db467c321c5b8523705914593145949da
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-03-26 12:17:10 +0700

    .commit

--- Home.py
+++ Home.py
@@ -18,14 +18,14 @@ html_8 = """
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-dt=pd.read_csv("./data/iris.csv")
+dt=pd.read_excel("./data/NewData.xlsx")
 
 st.write(dt.head(10))
 
-dt1 = dt['petal.length'].sum()
-dt2 = dt['petal.width'].sum()
-dt3 = dt['sepal.length'].sum()
-dt4 = dt['sepal.width'].sum()
+dt1 = dt['L-CORE'].sum()
+dt2 = dt['L-SURF'].sum()
+dt3 = dt['L-O2'].sum()
+dt4 = dt['L-BP'].sum()
 
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
@@ -46,10 +46,10 @@ html_8 = """
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-pt_len=st.slider("กรุณาเลือกข้อมูล petal.length")
-pt_wd=st.slider("กรุณาเลือกข้อมูล petal.width")
-sp_len=st.number_input("กรุณาเลือกข้อมูล sepal.length")
-sp_wd=st.number_input("กรุณาเลือกข้อมูล sepal.width")
+pt_len=st.slider("กรุณาเลือกข้อมูล L-CORE")
+pt_wd=st.slider("กรุณาเลือกข้อมูล L-SURF")
+sp_len=st.number_input("กรุณาเลือกข้อมูล L-O2")
+sp_wd=st.number_input("กรุณาเลือกข้อมูล L-BP")
 
 if st.button("ทำนายผล"):
    st.markdown("ใส่โมเดล")
